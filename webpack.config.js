bc685f9f3fdb6301e7dcc70f8c73d03ddc8da89f
Mario Jaramillo <mariojaramillo15@gmail.com>
2023-02-09 21:24:42 -0500

    uso y compilacion de webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,26 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: path.resolve(__dirname, './src/index.js'),
-  module: {
-    rules: [
-      {
-        test: /\.(js)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js']
-  },
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: path.resolve(__dirname, './dist'),
-  },
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    module: {
+        rules: [
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                  "style-loader",
+                  "css-loader",
+                  "sass-loader",
+              ]
+            },
+            { 
+              test: /\.js$/, 
+              exclude: /node_modules/, 
+              loader: "babel-loader" 
+            },
+        ],
+    },
 };
